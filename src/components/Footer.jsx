2a628fedd3d378674c93e7636071e9647897097b
Monodeep Das <80527009+Monodeep-Das@users.noreply.github.com>
2023-03-05 14:34:28 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import timelessLogo from '../assets/timeless.png'
+
+const Footer = () => (
+  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+    <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+      <div className="flex flex-[0.25] justify-center items-center">
+        <img src={timelessLogo} alt="logo" className="w-32" />
+      </div>
+
+
+      <div className="flex flex-[0.25] justify-center items-center">
+        <p className="text-white text-right text-xs">
+          &copy;2023 All rights reserved
+        </p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import timelessLogo from '../assets/timeless.png'
+import { connectWallet } from '../Blockchain.Services'
+import { useGlobalState, truncate } from '../store'
+
+const Header = () => {
+  const [connectedAccount] = useGlobalState('connectedAccount')
+  return (
+    <nav className="w-4/5 flex md:justify-left justify-between items-left py-4 mx-auto">
+      <div className="md:flex-[0.5] flex-initial justify-left items-left">
+        <img
+          className="w-32 cursor-pointer"
+          src={timelessLogo}
+          alt="TradeMark Logo"
+        />
+      </div>
+
+
+      {connectedAccount ? (
+        <button
+          className="shadow-xl shadow-black text-white
+        bg-[#e32970] hover:bg-[#bd255f] md:text-xs p-2
+          rounded-full cursor-pointer"
+        >
+          {truncate(connectedAccount)}
+        </button>
+      ) : (
+        <button
+          className="shadow-xl shadow-black text-white
+        bg-[#e32970] hover:bg-[#bd255f] md:text-xs p-2
+          rounded-full cursor-pointer"
+          onClick={connectWallet}
+        >
+          Connect Wallet
+        </button>
+      )}
+    </nav>
+  )
+}
+
+export default Header
